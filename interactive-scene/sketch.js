0fb7770a4ea428f08c210bf6e945c927ca6b14b0
utsho-2025 <utsho5939@gmail.com>
2025-03-03 14:15:52 -0600

    commented all of the code and some minor adjustments

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -15,8 +15,12 @@ let p1x = 0;
 let p1y = 10;
 let p2x;
 let p2y = 10;
+let gameState = "start";
+let scoreVar1 = 0;
+let scoreVar2 = 0;
 
-function keyPressed() {
+
+function keyPressed() {//controls for the paddles
   if (keyCode === 83 && keyIsPressed&& p2y!==0) {
     p1y +=pSpeed;
 
@@ -35,13 +39,17 @@ function keyPressed() {
 
   }
 }
+function showText(){
+  textSize(50);
+  text(scoreVar1,0,0,windowWidth/2,windowHeight/2);
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
   p2x = windowWidth-10;
 }
 
-function circ1(){
-  circle(windowWidth/2,windowHeight/2,70);
+function circ1(){//the midfield circle
+  circle(windowWidth/2,windowHeight/2,100);
   fill("white");
 }
 function draw() {
@@ -51,44 +59,45 @@ function draw() {
   rect(p2x,p2y,10,80);//paddle 2
   rect(p1x,p1y,10,80);//paddle 1
 
-  
-  
-
-  
-  line(windowWidth/2,0,windowWidth/2,windowHeight);
-
+  showText();
   
 
+  
+  line(windowWidth/2,0,windowWidth/2,windowHeight);//line going through the middle
 
   
 
-  ellipse(cordX,cordY,10,10);
+
+  
+
+  ellipse(cordX,cordY,10,10);//the ball itself
   noFill();
 
 
 
 
   
-  if (cordX>=windowWidth-5||cordX<6){
+  if (cordX>=windowWidth-5||cordX<6){//horizontal boundaries
     dx = -dx;
     
   }
-  if (cordY>=windowHeight-5||cordY<5){
+  if (cordY>=windowHeight-5||cordY<5){//vertical boundaries
     dy = -dy;
     
   }
   // Paddle collision check for p1
   if (cordX >= p1x && cordX <= p1x + 10 && cordY >= p1y && cordY <= p1y + 80) {
     dx = -dx;
-    dy = map(cordY - (p1y + 40), -40, 40, -4, 4); // Angular bounce
+    dy = map(cordY - (p1y + 40), -40, 40, -4, 4); // used for balls bounce if the ball hits the side of the paddles
   }
   
   // Paddle collision check for p2
   if (cordX >= p2x - 10 && cordX <= p2x && cordY >= p2y && cordY <= p2y + 80) {
     dx = -dx;
-    dy = map(cordY - (p2y + 40), -40, 40, -4, 4); // Angular bounce
+    dy = map(cordY - (p2y + 40), -40, 40, -4, 4); // this remaps the dy variable so that if the ball hits the corner of the paddle it shoots off differently
   }
   
+  //movement
   cordX += dx;
   cordY += dy;
 

397b946668f5d261f9ee5e151b8d16aefdf8b14e
utsho-2025 <utsho5939@gmail.com>
2025-03-06 14:15:39 -0600

    added some more stuff

--- 03-bouncing-ball/sketch.js
+++ 03-bouncing-ball/sketch.js
@@ -1,45 +1,64 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Bouncing Ball Object Demo
 
 let ballArray = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spawnBall();
 }
 
 function draw() {
   background(220);
-  for (let ball of ballArray){
-    ball.x+=ball.dx;
-    ball.y += ball.dy;
-
-    if (ball.x>width){
-      ball.x +=width;
-
-    }
-    else if (ball.x<0){
-      ball.x += width;
-    }
+  moveBalls();
+  displayBall();
 
 
-    fill("red");
-    circle(ball.x,ball.y,ball.radius*2);
-  }
 }
-function mousePressed(){
+
+function mousePressed() {
   spawnBall();
-  
 }
-function spawnBall(){
+
+function spawnBall() {
   let someBall = {
     x: random(width),
     y: random(height),
-    radius : random(15,40),
-    dx: random(-5,5),
-    dy: random(-5,5),
+    radius: random(15, 40),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
   };
   ballArray.push(someBall);
+}
+function moveBalls(){
+  for (let ball of ballArray) {
+    //move ball
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //teleport around edge of screen
+    if (ball.x - ball.radius> width) {
+      //off right side
+      ball.x  = -ball.radius;
+    }
+    else if (ball.x +ball.radius< 0) {
+      ball.x = width+ ball.radius;
+    }
+    if (ball.y -ball.radius> height) {
+      //fell off the bottom
+      ball.y = -ball.radius;
+
+    }
+    else if (ball.y+ball.radius < 0) {
+      //off top
+      ball.y = height+ ball.radius;
+    }
+
+
+  }
+}
+function displayBall(){
+  //display ball
+  fill("red");
+  circle(ball.x, ball.y, ball.radius * 2);  
+  
 }

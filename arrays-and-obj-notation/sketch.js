2100062d1764e1ec8db474881f27274d2cfb9f1b
utsho-2025 <utsho5939@gmail.com>
2025-03-13 12:15:18 -0600

    failed attempt at making a grid

--- arrays-and-obj-notation/sketch.js
+++ arrays-and-obj-notation/sketch.js
@@ -9,10 +9,28 @@
 // snake game
 const boardwidth=10;
 const boardheight=10;
+
+let gridSize = {
+  x : 200,
+  y: 200,
+  amountX : width/200,
+  amountY : height/200,
+};
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  drawGrid();
+}
+
+
+function drawGrid(){
+  for (let x  =  0 ; x<gridSize.amountX;x++){
+    for (let y =0; y<gridSize.amountY;y++){
+      rect(x*gridSize.x, y*gridSize.y, gridSize.x, gridSize.y);
+    }
+  }
 }

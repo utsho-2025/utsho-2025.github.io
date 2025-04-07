24f90defe97491971a661c712130079a990626fd
utsho-2025 <utsho5939@gmail.com>
2025-04-06 21:41:17 -0600

    made the base movement function for my game with states and movement

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -17,8 +17,14 @@ let thePlayer = {
 const PLAYER = 1;
 let cols;
 let rows;
-let x;
-let y;
+let snake = [{
+  x:0,
+  y:0,
+}]
+let DIRECTIONSTATE = "right";
+
+// let x;
+// let y;
 
 
 function setup() {
@@ -32,7 +38,7 @@ function setup() {
     cellSize = height / SQUARE_DIMENSIONS;
   }
   grid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
-  grid[thePlayer.y][thePlayer.x] = PLAYER;
+  // grid[thePlayer.y][thePlayer.x] = PLAYER;
 
 
 }
@@ -41,46 +47,54 @@ function draw() {
   background(220);
 
   displayGrid();
-  shiftPositions();
+  // shiftPositions();
+  moveSnake();
 
 }
 
-function keyPressed() {
+// function keyPressed() {
 
-  if (key ==="e"){
-    grid = generateGrid(SQUARE_DIMENSIONS,SQUARE_DIMENSIONS);
-  }
-  if (key === "w") {
-    //move up
-    movePlayer(thePlayer.x, thePlayer.y - 1);
-  }
-  if (key === "s") {
-    //move down
-    movePlayer(thePlayer.x, thePlayer.y + 1);
-  }
-  if (key === "a") {
-    //move left
-    movePlayer(thePlayer.x - 1, thePlayer.y);
-  }
-  if (key === "d") {
-    //move right
-    movePlayer(thePlayer.x + 1, thePlayer.y);
-  }
-}
+//   if (key ==="e"){
+//     grid = generateGrid(SQUARE_DIMENSIONS,SQUARE_DIMENSIONS);
+//     [thePlayer.y][thePlayer.x] = PLAYER;
+//   }
+//   if (key === "w") {
+//     //move up
+//     shiftPositions(thePlayer.x, thePlayer.y - 1);
+//   }
+//   if (key === "s") {
+//     //move down
+//     shiftPositions(thePlayer.x, thePlayer.y + 1);
+//   }
+//   if (key === "a") {
+//     //move left
+//     shiftPositions(thePlayer.x - 1, thePlayer.y);
+//   }
+//   if (key === "d") {
+//     //move right
+//     shiftPositions(thePlayer.x + 1, thePlayer.y);
+//   }
+// }
 
 function displayGrid() {
   for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
     for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
-      if (grid[y][x] === 1) {
+      if (snakeId(x,y)){
         fill("black");
       }
-      else if (grid[y][x] === 0) {
-        fill("white");
+      else{
+        fill("white")
+
+
       }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
+
+
     }
+      
   }
 }
+
 
 function generateGrid(cols, rows) {
   let newGrid = [];
@@ -93,18 +107,80 @@ function generateGrid(cols, rows) {
   return newGrid;
 }
 
-function shiftPositions(x,y){
-  if (x >= 0 && x < cols && y >= 0 && y <= rows && grid[y][x] === 0){
-    let oldX = x;
-    let oldY = y;
+// function shiftPositions(x,y){
+//   if (x >= 0 && x < SQUARE_DIMENSIONS && y >= 0 && y <= SQUARE_DIMENSIONS && grid[y][x] === 0){
+//     if(grid[y][x]===0){
+//       grid[thePlayer.y][thePlayer.x]===0;
+    
+//       thePlayer.x = x;
+//       thePlayer.y = y;
+  
+//       grid[oldY][oldX] = 0;
+  
+//       grid[thePlayer.y][thePlayer.x] = PLAYER;
+
+//     }
     
 
-    thePlayer.x = x;
-    thePlayer.y = y;
 
-    grid[oldY][oldX] = 0;
-
-    grid[thePlayer.y][thePlayer.x] = PLAYER;
+//   }
+// }
+function moveSnake(){
+  let snakeHead = {...snake[0]};// added this so in future if i add something complex to my object it wont throw an error
+  if (DIRECTIONSTATE=== "right"){
+    snakeHead.x +=1
 
   }
+  if (DIRECTIONSTATE=== "left"){
+    snakeHead.x -=1
+
+  }
+  if (DIRECTIONSTATE=== "up"){
+    snakeHead.y +=1
+
+  }
+  if (DIRECTIONSTATE=== "down"){
+    snakeHead.y -=1
+
+  } 
+
+  snake.unshift(snakeHead);
+
+  snake.pop();
+
+
+}
+function keyPressed(){
+  if ( key=== "d" && DIRECTIONSTATE !== "left"){
+    DIRECTIONSTATE = "right"
+
+  }
+
+  if ( key=== "a" && DIRECTIONSTATE !== "right"){
+    DIRECTIONSTATE = "left"
+
+  }
+  
+  if ( key=== "w" && DIRECTIONSTATE !== "down"){
+    DIRECTIONSTATE = "up"
+
+  }
+  
+  if ( key=== "s" && DIRECTIONSTATE !== "up"){
+    DIRECTIONSTATE = "down"
+
+  }
+
+}
+function snakeId(x,y){
+  for (let i = 0; i<snake.length; i++){
+    if (snake[i].x===x&&snake[i].y ===y){
+      return true;
+
+    }
+    
+
+  }
+  return false; 
+  
 }
